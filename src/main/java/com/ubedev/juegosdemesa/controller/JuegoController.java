3cafb24666f263b0120a0e5e85c56248be114beb
Juan Mendez <omailovin@gmail.com>
2023-09-04 20:58:29 +0200

    cambiamos FormJuego y JuegoController

--- src/main/java/com/ubedev/juegosdemesa/controller/JuegoController.java
+++ src/main/java/com/ubedev/juegosdemesa/controller/JuegoController.java
@@ -1,6 +1,12 @@
 package com.ubedev.juegosdemesa.controller;
 
 import com.ubedev.juegosdemesa.entity.Juego;
+import com.ubedev.juegosdemesa.services.JuegoService;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,12 +14,28 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class JuegoController {
 
+	@Autowired
+	private JuegoService js;
+	
+	
     @RequestMapping("/formJuego")
     public String muestraFormularioJuego(Model modelo) {
 
         Juego juegoNuevo = new Juego();
+        juegoNuevo.setId(1);
+        juegoNuevo.setNombre("Parchis Dummy");
+        
+        Juego juegoNuevo2 = new Juego();
+        juegoNuevo2.setId(2);
+        juegoNuevo2.setNombre("Oca Dummy");
+        
+        
+        //List<Juego> juegos = Arrays.asList(juegoNuevo,juegoNuevo2);
+        List<Juego> juegos = js.getJuegos();
+        
         System.out.println("Entro en el controlador de juego");
-        modelo.addAttribute("juegoNuevo", juegoNuevo);
+        modelo.addAttribute("juegos", juegos);
+        modelo.addAttribute("saludo", "Que pasa Rafita!!");
 
         return "juego/FormJuego";
     }
--- src/main/java/com/ubedev/juegosdemesa/services/JuegoService.java
+++ src/main/java/com/ubedev/juegosdemesa/services/JuegoService.java
@@ -2,8 +2,11 @@ package com.ubedev.juegosdemesa.services;
 
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
 import com.ubedev.juegosdemesa.entity.Juego;
 
+@Service
 public interface JuegoService {
 
 	public List<Juego> getJuegos(); 
