76edf27a4a62c98a13ec1dbbe5d96246244de637
Juan Mendez <omailovin@gmail.com>
2023-09-15 12:38:41 +0200

    formulario edicion juegos y tratamiento

--- src/main/java/com/ubedev/juegosdemesa/controller/JuegoController.java
+++ src/main/java/com/ubedev/juegosdemesa/controller/JuegoController.java
@@ -9,6 +9,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -21,13 +23,13 @@ public class JuegoController {
     @RequestMapping("/formJuego")
     public String muestraFormularioJuego(Model modelo) {
 
-        Juego juegoNuevo = new Juego();
-        juegoNuevo.setId(1);
-        juegoNuevo.setNombre("Parchis Dummy");
-        
-        Juego juegoNuevo2 = new Juego();
-        juegoNuevo2.setId(2);
-        juegoNuevo2.setNombre("Oca Dummy");
+		/*
+		 * Juego juegoNuevo = new Juego(); juegoNuevo.setId(1);
+		 * juegoNuevo.setNombre("Parchis Dummy");
+		 * 
+		 * Juego juegoNuevo2 = new Juego(); juegoNuevo2.setId(2);
+		 * juegoNuevo2.setNombre("Oca Dummy");
+		 */
         
         
         //List<Juego> juegos = Arrays.asList(juegoNuevo,juegoNuevo2);
@@ -39,4 +41,21 @@ public class JuegoController {
 
         return "juego/FormJuego";
     }
+    
+    
+    @RequestMapping("/edicionJuego")
+    public String muestraFormularioEdicionJuego(Model modelo) {
+    	Juego juegoNuevo=new Juego();
+    	modelo.addAttribute("juegoNuevo", juegoNuevo);
+        return "juego/FormEdicionJuego";
+    }
+    
+    @PostMapping("/procEdicionJuego")
+    public String procesaFormularioEdicionJuego(@ModelAttribute("juegoNuevo") Juego juegoNuevo) {
+    	
+    	System.out.println(juegoNuevo.getNombre());
+    	
+        return "juego/procEdicionJuego";
+    }
+
 }
